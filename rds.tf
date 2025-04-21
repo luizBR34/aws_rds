98479443d824aa5c63bba10f2df35c570e0d3276
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2025-04-20 21:28:22 -0300

    fixed issue

--- rds.tf
+++ rds.tf
@@ -19,22 +19,22 @@ resource "aws_db_parameter_group" "levelup-mariadb-parameters" {
 
 #RDS Instance properties
 resource "aws_db_instance" "levelup-mariadb" {
-  allocated_storage       = 20             # 20 GB of storage
+  allocated_storage       = 20            
   engine                  = "mariadb"
   engine_version          = "11.4.4"
-  instance_class          = "db.t2.micro"  # use micro if you want to use the free tier
+  instance_class          = "db.t3.micro"  
   identifier              = "mariadb"
   db_name                 = "mariadb"
-  username                = "root"           # username
-  password                = "mariadb141"     # password
+  username                = "root"         
+  password                = "mariadb141"    
   db_subnet_group_name    = aws_db_subnet_group.mariadb-subnets.name
   parameter_group_name    = aws_db_parameter_group.levelup-mariadb-parameters.name
-  multi_az                = false            # set to true to have high availability: 2 instances synchronized with each other
+  multi_az                = false           
   vpc_security_group_ids  = [aws_security_group.allow-mariadb.id]
-  storage_type            = "gp2"
-  backup_retention_period = 30                                          # how long you’re going to keep your backups
-  availability_zone       = aws_subnet.levelupvpc-private-1.availability_zone # prefered AZ
-  skip_final_snapshot     = true                                        # skip final snapshot when doing terraform destroy
+  storage_type            = "gp3"
+  backup_retention_period = 30                                        
+  availability_zone       = aws_subnet.levelupvpc-private-1.availability_zone 
+  skip_final_snapshot     = true                                        
   
   tags = {
     Name = "levelup-mariadb"
